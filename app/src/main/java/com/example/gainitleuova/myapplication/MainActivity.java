1497b3bc661a1efb10bb67b2ddfe2e5d275c0986
Gaini.Tleuova <Gaini.Tleuova@ico.kz>
2018-08-24 16:05:41 +0600

    first commit

---
+++ app/src/main/java/com/example/gainitleuova/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.gainitleuova.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl("https://rawgit.com/startandroid/data/master/messages/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ServerApi serverApi = retrofit.create(ServerApi.class);
+        Call<List<Message>> messages=serverApi.messages();
+        messages.enqueue(new Callback<List<Message>>() {
+            @Override
+            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                if(response.isSuccessful()){
+                    Log.d( "onResponse: ", response.body().toString());
+                }
+                else{
+                    Log.d( "onResponse: ", String.valueOf(response.code()));
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Message>> call, Throwable t) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/gainitleuova/myapplication/Message.java
@@ -0,0 +1,40 @@
+package com.example.gainitleuova.myapplication;
+
+public class Message {
+    private long id;
+    private long time;
+    private String text;
+    private String image;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
